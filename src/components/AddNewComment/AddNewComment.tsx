c0570c181a23cf5562ce1240f52b1f8f7fc1b051
Sesile26 <37846431+Sesile26@users.noreply.github.com>
2022-02-22 05:43:59 +0200

    fix

---
+++ src/components/AddNewComment/AddNewComment.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './AddNewComment.scss';
+
+type Props = {
+  addComment: (body: string) => Promise<void>,
+};
+
+export const AddNewComment: React.FC<Props> = ({ addComment }) => {
+  const [comment, setComment] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (comment) {
+      addComment(comment);
+      setComment('');
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="commentForm"
+    >
+      <input
+        type="text"
+        placeholder="Write something here"
+        value={comment}
+        onChange={(event) => setComment(event.target.value)}
+        className="input is-info"
+      />
+
+      <button
+        type="submit"
+        className="button is-success"
+      >
+        Add comment
+      </button>
+    </form>
+  );
+};
---
+++ src/components/AddNewPost/AddNewPost.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNewPost } from '../../api/posts';
+import { Reload } from '../../store/actions';
+import './AddNewPost.scss';
+
+export const AddNewPost: React.FC = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const clearForm = () => {
+    setTitle('');
+    setBody('');
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (title && body) {
+      await addNewPost(title, body);
+      clearForm();
+      dispatch(Reload());
+    }
+  };
+
+  return (
+    <form
+      className="content postForm"
+      onSubmit={handleSubmit}
+    >
+      <h2>Add new post</h2>
+      <input
+        type="text"
+        className="input postForm__input"
+        placeholder="Title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <textarea
+        className="textarea PostForm__textarea"
+        placeholder="Write here..."
+        value={body}
+        onChange={(event) => setBody(event.target.value)}
+      />
+      <button
+        type="submit"
+        className="button is-primary PostForm__button"
+      >
+        Post
+      </button>
+    </form>
+  );
+};
