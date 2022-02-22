73fd4303f70cecb79104098fb5e5d7436a2a5748
Dmytro Shershakov <37846431+Sesile26@users.noreply.github.com>
2022-02-22 06:00:20 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'plugin:react/recommended',
   ],
   rules: {
+    'linebreak-style': ['error', 'windows'],
     // React
     'react/prop-types': 0,
     'react/self-closing-comp': 0,
